2c702d2d3a0d4ec54d43971649f39adf4614aa43
Mamut <Mamut3D@ics.muni.cz>
2016-09-13 13:23:30 +0200

    minor test fixes

--- app/models/base.rb
+++ app/models/base.rb
@@ -24,13 +24,6 @@ class Base
     end.compact
   end
 
-  def service_ids_old(appliance_id, appdb_data)
-    appdb_data.select! do |site|
-      !site['appliances'].select! { |appliance| appliance['id'] == appliance_id }.blank?
-    end
-    appdb_data.collect { |service| service['id'] }
-  end
-
   def app_and_serv_check(appliance_id, service_id, appdb_data)
     service_ids = service_ids(appliance_id, appdb_data)
     raise Errors::NotFoundError, "Appliance with ID '#{appliance_id}' " \
